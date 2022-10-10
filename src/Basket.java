488fe2462663b816c01032bbcddc6a3a81aa3515
= <zaroslikob@mail.ru>
2022-10-11 00:38:35 +0300

    добавил метод

--- src/Basket.java
+++ src/Basket.java
@@ -1,53 +1,29 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 public class Basket {
     String[] products;
     int[] prices;
-
+    int[] quantity = new int[10];
+    int[] quantityProduct = new int[10];
     public Basket (String[] products, int[] prices){
         this.prices = prices;
         this.products = products;
     }
 
+
    public void addToCart(int productNum, int amount){
-       try {
-           String[] parts = input.split(" ");
-           if (parts.length != 2) {
-               System.out.println("Нужно ввести два числа на одной строке");
-               continue;
-           }
 
-           int product = Integer.parseInt(parts[0]);
-
-           if ((0 >= product) || (product >= products.length)) {
-               System.out.println("В параметре надо указать коректный товар " + product);
-               continue;
-           }
-
-           product--;
-
-           int pricesOne = Integer.parseInt(parts[1]);
-           if (pricesOne <= 0) {
-               System.out.println("Введите положительное число " + pricesOne);
-               continue;
-           }
-
-           if (product < (products.length + 1) && product >= 0) {
-               quantity[product] += pricesOne;
-               int currentPrice = prices[product];
-               quantityProduct[product] = quantity[product] * currentPrice;
-           } else {
-               System.out.println("Такого товара нет");
-           }
-       } catch (NumberFormatException e) {
-           System.out.println("Нужно ввести два числа, а вы ввели " + input);
-           continue;
+       if (productNum < (products.length + 1) && productNum >= 0) {
+           quantity[productNum] += amount;
+           int currentPrice = prices[productNum];
+           quantityProduct[productNum] = quantity[productNum] * currentPrice;
+       } else {
+           System.out.println("Такого товара нет");
        }
    }
-
-    }
 
 //        public void printCart(){
 //            System.out.println("Список возможных товаров для покупки");
--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,11 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        File textFile = new File("src/textFile.txt");
-        PrintWriter out = new PrintWriter(textFile);
-        out.println();
-        out.close();
-
-        InputStream in = new FileInputStream(textFile);
-        Scanner scanner1 = new Scanner(in);
-        String name = scanner1.nextLine();
-        String[] asd = scanner1.nextLine().split("");
-
-        Scanner scanner = new Scanner(System.in);
-
         String[] products = {"Хлеб", "Яблоки", "Молоко"};
         int[] prices = {100, 200, 300};
-        Basket basket = new Basket(products,prices);
+        Basket basket = new Basket(products, prices);
 
-        System.out.println("Список возможных товаров для покупки");
-        for (int i = 0; i < products.length; i++) {
-            System.out.println((i + 1) + "." + " " + products[i] + " " + prices[i] + " руб/шт");
-        }
-
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.println("Выберите товар и количество или введите `end`");
             String input = scanner.nextLine();
@@ -41,7 +25,60 @@ public class Main {
                 break;
             }
 
+            try {
+                String[] parts = input.split(" ");
+                if (parts.length != 2) {
+                    System.out.println("Нужно ввести два числа на одной строке");
+                    continue;
+                }
+
+                int product = Integer.parseInt(parts[0]);
+
+                if ((0 >= product) || (product >= products.length)) {
+                    System.out.println("В параметре надо указать коректный товар " + product);
+                    continue;
+                }
+
+                product--;
+
+                int pricesOne = Integer.parseInt(parts[1]);
+                if (pricesOne <= 0) {
+                    System.out.println("Введите положительное число " + pricesOne);
+                    continue;
+                }
+
+                basket.addToCart(product,pricesOne);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Нужно ввести два числа, а вы ввели " + input);
+                continue;
+            }
         }
     }
 }
 
+
+if (product < (products.length + 1) && product >= 0) {
+        quantity[product] += pricesOne;
+        int currentPrice = prices[product];
+        quantityProduct[product] = quantity[product] * currentPrice;
+        } else {
+        System.out.println("Такого товара нет");
+
+
+
+
+
+
+
+
+//    File textFile = new File("src/textFile.txt");
+//    PrintWriter out = new PrintWriter(textFile);
+//        out.println();
+//        out.close();
+//
+//    InputStream in = new FileInputStream(textFile);
+//    Scanner scanner1 = new Scanner(in);
+//    String name = scanner1.nextLine();
+//    String[] asd = scanner1.nextLine().split("");
+//
