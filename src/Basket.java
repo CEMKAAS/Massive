0831dd0fb9e7017779b5e781cf91cc484f9814ec
= <zaroslikob@mail.ru>
2022-10-17 21:29:13 +0300

    Реформат кода исправление ошибок

--- src/Basket.java
+++ src/Basket.java
@@ -9,16 +9,18 @@ public class Basket implements Serializable {
     private int[] quantityProduct;
 
 
-    public Basket (String[] products, int[] prices) throws FileNotFoundException {
+    public Basket(String[] products, int[] prices) throws FileNotFoundException {
         this.prices = prices;
         this.products = products;
-        this.quantity= new int[prices.length];
+        this.quantity = new int[prices.length];
         this.quantityProduct = new int[prices.length];
     }
-    public void setCount(int[] quantity) {
+
+    public void setQuantity(int[] quantity) {
         this.quantity = quantity;
     }
-    public void setCount2(int[] quantityProduct) {
+
+    public void setQuantityProduct(int[] quantityProduct) {
         this.quantityProduct = quantityProduct;
     }
 
@@ -30,36 +32,36 @@ public class Basket implements Serializable {
         return quantityProduct;
     }
 
-    public void addToCart(int productNum, int amount){
-       if (productNum < (products.length + 1) && productNum >= 0) {
-           quantity[productNum] += amount;
-           int currentPrice = prices[productNum];
-           quantityProduct[productNum] = quantity[productNum] * currentPrice;
-       } else {
-           System.out.println("Такого товара нет");
-       }
-   }
+    public void addToCart(int productNum, int amount) {
+        if (productNum < (products.length + 1) && productNum >= 0) {
+            quantity[productNum] += amount;
+            int currentPrice = prices[productNum];
+            quantityProduct[productNum] = quantity[productNum] * currentPrice;
+        } else {
+            System.out.println("Такого товара нет");
+        }
+    }
 
-    protected void printCart () {
+    protected void printCart() {
 
-            StringBuilder print = new StringBuilder();
-            print.append("Ваша корзина");
-            print.append("\n");
-            for (int i = 0; i < products.length; i++) {
-                if (quantity[i] >= 1) {
-                    print.append(products[i] + " " + quantity[i] + " шт " + prices[i] + " руб/шт " + quantityProduct[i] + " руб в сумме ");
-                    print.append("\n");
-                    setCount(quantity);
-                    setCount2(quantityProduct);
-                }
+        StringBuilder print = new StringBuilder();
+        print.append("Ваша корзина");
+        print.append("\n");
+        for (int i = 0; i < products.length; i++) {
+            if (quantity[i] >= 1) {
+                print.append(products[i] + " " + quantity[i] + " шт " + prices[i] + " руб/шт " + quantityProduct[i] + " руб в сумме ");
+                print.append("\n");
+                setCount(quantity);
+                setCount2(quantityProduct);
             }
-            print.append("Итого: " + Arrays.stream(quantityProduct).sum() + " руб");
+        }
+        print.append("Итого: " + Arrays.stream(quantityProduct).sum() + " руб");
 
         System.out.println(print.toString());
-        }
+    }
 
-    public void saveBin(File binFile ) {
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(binFile ))) {
+    public void saveBin(File binFile) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(binFile))) {
             objectOutputStream.writeObject(this);
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,8 +72,6 @@ public class Basket implements Serializable {
         try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(binFile))) {
             Basket basket = (Basket) objectInputStream.readObject();
             System.out.print("В вашей корзине было" + "\n");
-            basket.getQuantity();
-            basket.getQuantityProduct();
             basket.printCart();
             return basket;
         }
